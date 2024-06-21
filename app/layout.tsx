74b16ab2f0dfdf3efdebd836aeeb3b6b22b0311b
Arafat <arafatahmedtanimcsedu57@gmail.com>
2024-06-21 19:28:38 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import GoogleAdsense from "./components/GoogleAdsense";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,12 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9101987783305090"
-        crossOrigin="anonymous"
-      ></script>
-
+      <GoogleAdsense pId="9101987783305090" />
       <body className={inter.className}>{children}</body>
     </html>
   );
